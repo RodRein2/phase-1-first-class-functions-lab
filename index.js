a33c57f6259ba31cff368f841129672bbb3625a1
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-12 20:18:43 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function(fare) {
+        return fare * integer;
+    }
+}
+
+function fareDoubler(fare) {
+    const fareMultiplier = createFareMultiplier(2);
+    return fareMultiplier(fare);
+}
+function fareTripler(fare) {
+    const fareMultiplier = createFareMultiplier(3);
+    return fareMultiplier(fare);
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
